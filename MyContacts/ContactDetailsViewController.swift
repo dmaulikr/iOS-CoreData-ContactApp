d0726718f669eca7e9103eeb3152f95d29d32a00
Yawo Echitey <yawo@echitey.com>
2017-04-05 17:10:31 -0300

    Added dataSource to PickerView

--- MyContacts/ContactDetailsViewController.swift
+++ MyContacts/ContactDetailsViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import CoreData
 
-class ContactDetailsViewController: UIViewController {
+class ContactDetailsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+    @IBOutlet weak var typePicker: UIPickerView!
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var phoneTxt: UITextField!
+    @IBOutlet weak var contactImageView: UIImageView!
+    
+    var types = [ContactType]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetchContactTypes()
+        
+        typePicker.dataSource = self
+        typePicker.delegate = self
 
         if let topItem = self.navigationController?.navigationBar.topItem{
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return types.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let type = types[row]
+        return type.type
+    }
+    
+    @IBAction func saveContact(_ sender: UIBarButtonItem){
+        print("SAVING ITEM")
+    }
+    
+    func fetchContactTypes(){
+        let request:  NSFetchRequest<ContactType> = ContactType.fetchRequest()
+        
+        do{
+            self.types = try context.fetch(request)
+        }catch{
+            //Error handling
+            print("An Error occured during operation!")
         }
     }
 }
